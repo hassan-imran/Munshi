acf67c95ea16ff14eb7767239870e1dde553aaf6
hassan-imran <43490649+hassan-imran@users.noreply.github.com>
2019-01-19 19:27:48 +0500

    delet and add new category working

--- home/firebaseLink.js
+++ home/firebaseLink.js
@@ -2,18 +2,18 @@
 
 (() => {
     document.getElementById('signoutBtn').addEventListener('click', (e) => {
-        firebase.auth().signOut().then(
-            () => {
-                window.location.href = '../index.html';
-                alert("You have signed out!");
-            }
-        )
-            .catch(
-                (e) => {
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log(errorCode, errorMessage);
+            firebase.auth().signOut().then(
+                () => {
+                    window.location.href = '../index.html';
+                    alert("You have signed out!");
                 }
             )
+                .catch(
+                    (e) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorCode, errorMessage);
+                    }
+                )
     });
 })();
