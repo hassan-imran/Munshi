03e955630149fdf6eefe6d4710b48df3d1467e49
hassan-imran <43490649+hassan-imran@users.noreply.github.com>
2019-01-27 11:01:47 +0500

    more functionality on the home/index page

--- home/scripts/index.js
+++ home/scripts/index.js
@@ -1,6 +1,7 @@
 document.getElementById('stores').innerHTML = Object.keys(stores).length;
 document.getElementById('low-stock').innerHTML = lowStock();
 document.getElementById('discounted').innerHTML = discounted();
+document.getElementById('total-sales').innerHTML = totalSales();
 
 function lowStock() {
     var i = 0;
@@ -17,6 +18,14 @@ function discounted() {
         if ((products[name].discount) > 0) {
             i++;
         }
+    }
+    return i;
+}
+
+function totalSales() {
+    var i = 0;
+    for (name in products) {
+        i += products[name].sold;
     }
     return i;
 }
